7cb8663960588fb131f71a00859e15d81ed04f83
Indy Iri <indy.iri@student.pxl.be>
2024-10-15 22:30:25 +0200

    WPF layout gemaakt

---
+++ H4Oef1Raadspel/H4Oef1Raadspel/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace H4Oef1Raadspel
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
