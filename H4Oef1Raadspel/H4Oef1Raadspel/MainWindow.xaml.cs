43d82b4322a03914b1827d61f886bde41663b417
Indy Iri <indy.iri@student.pxl.be>
2024-10-17 21:44:54 +0200

    Random nummers willen niet genereren en de 'amountOfGuesses wilt niet vanaf 0 beginnen

--- H4Oef1Raadspel/H4Oef1Raadspel/MainWindow.xaml.cs
+++ H4Oef1Raadspel/H4Oef1Raadspel/MainWindow.xaml.cs
@@ -21,19 +21,53 @@ namespace H4Oef1Raadspel
             InitializeComponent();
         }
 
+        Random rnd = new Random();
+        int number = 0;
+        int amountOfGuesses = 0;
+        int randomNumber;
+
         private void endButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
         }
 
-        private void evaluateButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void newLabel_Click(object sender, RoutedEventArgs e)
         {
-
+            int randomNumber = rnd.Next(1, 101);
+            numberTextBox.Focus();
+            output1TextBox.Clear();
+            output2TextBox.Clear();
         }
+
+        private void evaluateButton_Click(object sender, RoutedEventArgs e)
+        {
+            string inputNumber = numberTextBox.Text;
+            bool isInputNumberValid = int.TryParse(inputNumber, out number);
+
+            if (isInputNumberValid && number <= 100 && number >= 0)
+            {
+               
+                    if (number < randomNumber)
+                    {
+                        output1TextBox.Text = "Raad hoger!";
+                    }
+                    if (number > randomNumber)
+                    {
+                        output1TextBox.Text = "Raad lager";
+                    }
+                    if (number == randomNumber)
+                    {
+                        output1TextBox.Text = "Proficiat! U hebt het getal geraden";
+                    }
+                    amountOfGuesses++;
+                    output2TextBox.Text = $"Aantal keer geraden: {amountOfGuesses}";                
+            }
+            else
+            {
+                output1TextBox.Text = "Geef een getal in tussen 1 en 100";
+                output2TextBox.Text = "Geef een getal in tussen 1 en 100";
+                numberTextBox.Focus();               
+            }
+        }      
     }
 }
